d63c5833b2013a70537d4dac41ea5ae5401b3a50
Timothyw0 <timothyw0@gmail.com>
2023-05-16 20:49:59 -0400

    Fix navbar styling issue again.

--- frontend/wang-family-tree/src/components/Navbar/Navbar.tsx
+++ frontend/wang-family-tree/src/components/Navbar/Navbar.tsx
@@ -4,13 +4,14 @@ import { ChevronDownIcon } from "@chakra-ui/icons";
 import { changeLanguage } from "../../store/languageSlice";
 import { useLanguageSelector } from "../../hooks/useLanguageSelector";
 import { useAppDispatch } from "../../hooks/reduxHooks";
+import "./Navbar.css";
 
 const Navbar = () => {
   const textLang = useLanguageSelector("navbar");
   const dispatch = useAppDispatch();
 
   return (
-    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-cyan-500">
+    <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-cyan-500 wang-navbar">
       <div className="container px-4 mx-auto flex items-center justify-between">
         <div className="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start">
           <span className="lg:text-3xl sm:text-sm font-bold leading-relaxed inline-block py-2 whitespace-nowrap uppercase text-white">

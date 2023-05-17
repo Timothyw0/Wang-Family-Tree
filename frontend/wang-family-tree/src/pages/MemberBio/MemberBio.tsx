d99020c716831cd68991c63b0982e0173f271464
Timothyw0 <timothyw0@gmail.com>
2023-05-17 11:55:05 -0400

    Add easter egg. Move no picture avatar to React base

--- frontend/wang-family-tree/src/pages/MemberBio/MemberBio.tsx
+++ frontend/wang-family-tree/src/pages/MemberBio/MemberBio.tsx
@@ -6,18 +6,20 @@ import { useLanguageSelector } from "../../hooks/useLanguageSelector";
 
 const Navbar = lazy(() => import("../../components/Navbar"));
 
-const MemberBio = () => {
-  const [memberData, setMemberData] = useState<any>({});
+const MemberBio = ({ data = {} }) => {
+  const [memberData, setMemberData] = useState<any>(data);
   const { memberId } = useParams();
   const { language } = useLanguageSelector();
 
+  const noPicture =
+    "https://lh3.googleusercontent.com/9s-zOPXFh6SnHz27ihgPr24IToqBQ1_9tdlY3XdVj5VKXg37sBkI0chIRMfn5J-j8i2MkiY8KqtOfnV5mxkb9bbq-kCyGcTuiJtGMlS14jwYPIFEiPVvxcPSz4uCu_z79uXDOyaA_g=w2400";
+
   useEffect(() => {
-    apiService.getPerson(memberId || "0").then(({ data }) => {
+    if (!memberId) return;
+    apiService.getPerson(memberId).then(({ data }) => {
       setMemberData(data);
     });
   }, []);
-
-  console.log(memberData);
 
   return (
     <Suspense fallback="">
@@ -26,7 +28,7 @@ const MemberBio = () => {
         <Card maxW="sm" className="m-auto content-center">
           <CardBody>
             <Image
-              src={memberData?.photoURL}
+              src={memberData?.photoURL || noPicture}
               borderRadius="lg"
               referrerPolicy="no-referrer"
             />
--- frontend/wang-family-tree/src/routes/Routes.tsx
+++ frontend/wang-family-tree/src/routes/Routes.tsx
@@ -15,6 +15,21 @@ const Routes: React.FC = () => {
       <BrowserRouter>
         <RouterRoutes>
           <Route path="/" element={<Home />} />
+          <Route
+            path="/eduardo"
+            element={
+              <MemberBio
+                data={{
+                  photoURL:
+                    "https://lh3.googleusercontent.com/JF1Lj3W3iE-Wr4uhJ3Nf9qakjZMM-wO1b1iM6N21xGnxhu5i2VtNK8G0M0s-JTE5IFPUt-C1XumEXWljydWkvRsg8iSsNQAi3G0l11L678SIqhZbTnc9C4ERXh-xiOFeZXAVbCSIAw=w2400",
+                  name: {
+                    english: "Eduardo",
+                    chinese: "Eduardo",
+                  },
+                }}
+              />
+            }
+          />
           <Route path="/:memberId" element={<MemberBio />} />
           <Route path="*" element={<Navigate to="/" />} />
         </RouterRoutes>

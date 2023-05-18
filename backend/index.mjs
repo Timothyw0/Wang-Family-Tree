3ec64b2ea75ab03288e65e0460bd0e16bf8b2452
Timothyw0 <timothyw0@gmail.com>
2023-05-17 20:10:16 -0400

    Try serving React directly from heroku

--- backend/index.mjs
+++ backend/index.mjs
@@ -1,12 +1,22 @@
 import express from "express";
 import cors from "cors";
 import family from "./routes/family.mjs";
+import { fileURLToPath } from "url";
+import path, { dirname } from "path";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static("dist"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.use("/family", family);
 
--- frontend/wang-family-tree/src/interfaces/axiosConfig.ts
+++ frontend/wang-family-tree/src/interfaces/axiosConfig.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://wang-family-tree.herokuapp.com",
+  // baseURL: "https://wang-family-tree.herokuapp.com",
+  baseURL: "",
   headers: {
     "Content-type": "application/json",
   },

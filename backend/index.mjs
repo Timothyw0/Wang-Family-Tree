6ad48b826a5a6602a64615b50ee57013296efe13
Timothyw0 <timothyw0@gmail.com>
2023-05-17 20:14:51 -0400

    Heroku fix

--- backend/index.mjs
+++ backend/index.mjs
@@ -12,10 +12,10 @@ const __dirname = dirname(__filename);
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static("dist"));
+app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.use("/family", family);

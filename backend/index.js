8f9f8112999d8efeafeb1a541e578005b37c66be
Timothyw0 <timothyw0@gmail.com>
2023-05-17 20:37:22 -0400

    One more time

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,6 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/", (req, res) => {
-  // hm
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
